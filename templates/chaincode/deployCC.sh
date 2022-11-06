6743ace90b4df88db987a56815bbbfacf002af30
Agustinus Theodorus <agustinustheoo@gmail.com>
2022-11-06 12:13:45 +0700

    feat: change chaincode to compatible v2.2 version

---
+++ templates/chaincode/deployCC.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+source scriptUtils.sh
+
+CHANNEL_NAME=${1:-"common.tx"}
+CC_NAME=${2:-"chaincode"}
+CC_SRC_PATH=${3:-"NA"}
+CC_SRC_LANGUAGE=${4:-"golang"}
+CC_VERSION=${5:-"1.0"}
+CC_SEQUENCE="1"
+CC_INIT_FCN=${7:-"NA"}
+CC_END_POLICY=${8:-"NA"}
+CC_COLL_CONFIG=${9:-"NA"}
+DELAY=${10:-"3"}
+MAX_RETRY=${11:-"5"}
+VERBOSE=${12:-"false"}
+
+println "executing with the following"
+println "- CHANNEL_NAME: ${C_GREEN}${CHANNEL_NAME}${C_RESET}"
+println "- CC_NAME: ${C_GREEN}${CC_NAME}${C_RESET}"
+println "- CC_SRC_PATH: ${C_GREEN}${CC_SRC_PATH}${C_RESET}"
+println "- CC_SRC_LANGUAGE: ${C_GREEN}${CC_SRC_LANGUAGE}${C_RESET}"
+println "- CC_VERSION: ${C_GREEN}${CC_VERSION}${C_RESET}"
+println "- CC_SEQUENCE: ${C_GREEN}${CC_SEQUENCE}${C_RESET}"
+println "- CC_END_POLICY: ${C_GREEN}${CC_END_POLICY}${C_RESET}"
+println "- CC_COLL_CONFIG: ${C_GREEN}${CC_COLL_CONFIG}${C_RESET}"
+println "- CC_INIT_FCN: ${C_GREEN}${CC_INIT_FCN}${C_RESET}"
+println "- DELAY: ${C_GREEN}${DELAY}${C_RESET}"
+println "- MAX_RETRY: ${C_GREEN}${MAX_RETRY}${C_RESET}"
+println "- VERBOSE: ${C_GREEN}${VERBOSE}${C_RESET}"
+
+CC_SRC_LANGUAGE=$(echo "$CC_SRC_LANGUAGE" | tr [:upper:] [:lower:])
+
+CC_SRC_PATH="."
+
+# do some language specific preparation to the chaincode before packaging
+CC_RUNTIME_LANGUAGE=golang
+
+INIT_REQUIRED="--init-required"
+# check if the init fcn should be called
+if [ "$CC_INIT_FCN" = "NA" ]; then
+  INIT_REQUIRED=""
+fi
+
+if [ "$CC_END_POLICY" = "NA" ]; then
+  CC_END_POLICY=""
+else
+  CC_END_POLICY="--signature-policy $CC_END_POLICY"
+fi
+
+if [ "$CC_COLL_CONFIG" = "NA" ]; then
+  CC_COLL_CONFIG=""
+else
+  CC_COLL_CONFIG="--collections-config $CC_COLL_CONFIG"
+fi
+
+# import utils
+. envVar.sh
+
+# installChaincode PEER ORG
+installChaincode() {
+  ORG=$1
+  setGlobals $ORG
+  set -x
+  peer lifecycle chaincode install ${CC_NAME}.tar.gz >&log.txt
+  res=$?
+  { set +x; } 2>/dev/null
+  cat log.txt
+  verifyResult $res "Chaincode installation on peer0.org${ORG} has failed"
+  successln "Chaincode is installed on peer0.org${ORG}"
+}
+
+# queryInstalled PEER ORG
+queryInstalled() {
+  ORG=$1
+  setGlobals $ORG
+  set -x
+  peer lifecycle chaincode queryinstalled >&log.txt
+  res=$?
+  { set +x; } 2>/dev/null
+  cat log.txt
+  PACKAGE_ID=$(sed -n "/${CC_NAME}_${CC_VERSION}/{s/^Package ID: //; s/, Label:.*$//; p;}" log.txt)
+  verifyResult $res "Query installed on peer0.org${ORG} has failed"
+  successln "Query installed successful on peer0.org${ORG} on channel"
+}
+
+## Install chaincode on peer0.org{{ org_id }}
+infoln "Installing chaincode on peer0.org{{ org_id }}..."
+installChaincode {{ org_id }}
+
+## query whether the chaincode is installed
+queryInstalled {{ org_id }}
+
+exit 0
